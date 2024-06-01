42a87e765cee4914ea5230e8193113c24a213a01
Yu Chan <tw34702477@gmail.com>
2024-06-01 23:56:02 +0800

    專題

--- Final_Project/object_renderer.py
+++ Final_Project/object_renderer.py
@@ -5,6 +5,15 @@ class ObjectRenderer:
     def __init__(self, game):
         self.game = game
         self.screen = game.screen
+        self.wall_textures = self.load_wall_textures()
+
+    def draw(self):
+        self.render_game_objects()
+
+    def render_game_objects(self):
+        list_objects = self.game.raycasting.objects_to_render
+        for depth, image, pos in list_objects:
+            self.screen.blit(image, pos)
     
     @staticmethod
     def get_texture(path, res = (TEXTURE_SIZE, TEXTURE_SIZE)):
@@ -14,8 +23,8 @@ class ObjectRenderer:
     def load_wall_textures(self):
         return {
             1: self.get_texture('resources/textures/1.png'),
-            2: self.get_texture('resources/textures/2.png'),
-            3: self.get_texture('resources/textures/3.png'),
-            4: self.get_texture('resources/textures/4.png'),
-            5: self.get_texture('resources/textures/5.png'),
+            # 2: self.get_texture('resources/textures/2.png'),
+            # 3: self.get_texture('resources/textures/3.png'),
+            # 4: self.get_texture('resources/textures/4.png'),
+            # 5: self.get_texture('resources/textures/5.png'),
         }
