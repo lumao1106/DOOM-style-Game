402c69bce8424301cfda56cba75540b7966d2d8f
Yu Chan <tw34702477@gmail.com>
2024-06-01 22:13:27 +0800

    專題

--- Final_Project/settings.py
+++ Final_Project/settings.py
@@ -1,8 +1,21 @@
+import math
+
 #game setting
-RES = width, height = 1200, 720
-FPS = 60
+RES = WIDTH, HEIGHT = 1200, 720
+HALF_WIDTH = WIDTH // 2
+HALF_HEIGHT = HEIGHT // 2
+FPS = 0
 
 PLAYER_POS = 1.5, 1 # mini_map
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
 PLAYER_ROT_SPEED = 0.002
+
+FOV = math.pi / 3
+HALF_FOV = FOV / 2
+NUM_RAYS = WIDTH // 2
+HALF_NUM_RAYS = NUM_RAYS // 2
+DELTA_ANGLE = FOV / NUM_RAYS
+MAX_DEPTH = 20
+
+SCREEN_DIST = HALF_WIDTH / math.tan(HALF_FOV)
